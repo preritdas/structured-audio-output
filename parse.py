46262361362fc4062e365a8130bb29d4fd453259
Prerit Das <preritdas@gmail.com>
2024-06-26 14:52:26 -0400

    Fix hardcoded keys issue.

--- parse.py
+++ parse.py
@@ -7,7 +7,7 @@ import json
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 
-PROMPT = """Take the following voice transcription and format it into the following categories: What they do, Problems solved, Case study, and My initial take.
+PROMPT = """Take the following voice transcription and format it into the following categories: {keys_desc}
 
 You ONLY respond in JSON format with the following keys: {keys_str}
 
@@ -20,7 +20,7 @@ Transcription:
 
 def create_prompt(keys: list[str], transcription: str) -> str:
     keys_str = str(keys)  # ex. '["What they do", "Problems solved", "Case study", "My initial take"]'
-    return PROMPT.format(keys_str=keys_str, transcription=transcription)
+    return PROMPT.format(keys_str=keys_str, transcription=transcription, keys_desc=", ".join(keys))
 
 
 def structured_output(keys: list[str], transcript: str) -> dict[str, str]:
